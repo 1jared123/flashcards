68a3058781712c5baf02b1a904eeccf17ca06211
Jared Moscrip <jaredmoscrip@gmail.com>
2017-04-11 15:41:33 -0500

    workings

--- cards.js
+++ cards.js
@@ -1,30 +1,29 @@
+var inquirer = require("inquirer");
+
+
 function BasicCard(front, back) {
   this.front = front;
   this.back = back;
-  this.makeCards = function() {
-    if (this.front === true) {
-      console.log(this.back);
-    }
-  };
-}
+};
 
 function ClozeCard(text, cloze) {
   this.text = text;
   this.cloze = cloze;
-  this.cloze = function() {
-    console.log(cloze);
-  };
+  this.clozes = console.log(cloze);
+
   this.partial = function() {
-    try {
+    // if in the text is the text in cloze, then continue, if not throw error.
+    if (text.search( cloze ) >= 0) {
      console.log(text.replace(cloze," ... "));
-    } catch (error) {
-     console.log("Error in the making of the card.")
+    } else {
+     console.log("Error in the making of the card.");
     }
   };
+  //console log the whole question
   this.fullText = function() {
     console.log(text);
   };
-}
+};
 
 
 var firstPresident = new BasicCard(
@@ -37,13 +36,13 @@ var firstPresident = new BasicCard(
 // console.log(firstPresident.back); 
 
 var firstPresidentCloze = new ClozeCard( //"This shouldn't work...", "We shall see");
-   "George Washington was the first president of the United States.", "George Washington");
+  "George Washington was the first president of the United States.", "George Washington");
 
 // "George Washington"
-// firstPresidentCloze.cloze(); 
+firstPresidentCloze.clozes; 
 
 // " ... was the first president of the United States.
-firstPresidentCloze.partial(); 
+// firstPresidentCloze.partial(); 
 
 // "George Washington was the first president of the United States.
 // firstPresidentCloze.fullText();
@@ -51,3 +50,52 @@ firstPresidentCloze.partial();
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
 // var brokenCloze("This doesn't work", "oops"); 
 
+inquirer.prompt([
+
+  // Here we create a basic text prompt.
+  {
+    type: "input",
+    message: firstPresidentCloze.partial(),
+    name: "name"
+  },
+
+  {
+    type: "confirm",
+    message: "You want the answer?",
+    name: "confirm",
+    default: true
+
+  }
+
+  
+
+]).then(function(user) {
+  if (user.name === true) {
+    firstPresidentCloze.cloze();
+    inquirer.prompt([
+
+    {
+      type: "confirm",
+      message: "Next question??",
+      name:"forsure",
+      default: true
+    }
+
+      ]).then(function(back) {
+        console.log("next question should be here.");
+    })
+  }
+
+})
+
+
+/*
+window.onload = function() {
+$(".question").append(firstPresident.front);
+$(document).on("click", ".next-one", function() {
+  // console.log(firstPresident.front); 
+  $(".question").append(firstPresident.back);
+});
+
+}
+*/
