e45e8502c379c835a40d16200d0df306fab5b6d0
Jared Moscrip <jaredmoscrip@gmail.com>
2017-04-12 19:10:40 -0500

    learned i was doing to much

--- cards.js
+++ cards.js
@@ -1,85 +1,6 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
 
-switch(process.argv[2]) {
-  case "create":
-    createCards();
-      break;
-
-  case "display-cloze":
-    displayCards();
-      break;
-
-  case "display-partial":
-    console.log("Not created yet");
-      break;
-
-  case "card-fullText":
-    console.log("Not created yet");
-      break;
-};
-
-var giveMeQuestions = [];
-
-function createCards() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "Enter question: ",
-      name: "question"
-     },
-
-     {
-      type: "input",
-      message: "Enter answer: ",
-      name: "answer"
-     }
-    ]).then(function(data){
-      var newcards = new BasicCard(data.question, data.answer);
-      // var buttermilk = newcards.front;
-      // var skimmilk = newcards.back;
-      // var functionmilk = newcards.partial();
-      // console.log(newcards);
-
-      giveMeQuestions.push(newcards);
-
-      inquirer.prompt([
-        {
-          type: "input",
-          message: newcards.text + "What's the answer?",
-          choices: "Press Enter to show answer."
-          name: "name"
-        },
-
-        ]).then(function(info) {
-          
-        })
-
-      // fs.appendFile("log.js", "[" + "'" + buttermilk + "'" + ", " + "'" + skimmilk + "'" + " " + "]" + "\n", function(error){
-      // fs.appendFile("log.js", newcards + "\n", function(error){
-      //   if (error) {
-      //     console.log(error);
-      //   } else {
-      //     console.log("You added a card!");
-      //   }        
-      // });
-    })
-};
-
-function displayCards() {
-      console.log(giveMeQuestions);
-
-  /*
-  fs.readFile("log.js", "utf8", function(error, data) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(data);
-    }
-  })
-  */
-};
-
 
 function BasicCard(front, back) {
   this.front = front;
@@ -103,29 +24,29 @@ function ClozeCard(text, cloze) {
   };
 };
 
-// var firstPresident = new BasicCard(
-    // "Who was the first president of the United States?", "George Washington");
+var firstPresident = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
 
 // "Who was the first president of the United States?"
-// console.log(firstPresident.front); 
+console.log(firstPresident.front); 
 
 // "George Washington"
-// console.log(firstPresident.back); 
+console.log(firstPresident.back); 
 
-// var firstPresidentCloze = new ClozeCard( //"This shouldn't work...", "We shall see");
-  // "George Washington was the first president of the United States.", "George Washington");
+var firstPresidentCloze = new ClozeCard( 
+  "George Washington was the first president of the United States.", "George Washington");
 
 // "George Washington"
-// firstPresidentCloze.clozes; 
+firstPresidentCloze.clozes; 
 
 // " ... was the first president of the United States.
-// firstPresidentCloze.partial(); 
+firstPresidentCloze.partial(); 
 
 // "George Washington was the first president of the United States.
-// firstPresidentCloze.fullText();
+firstPresidentCloze.fullText();
 
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-// var brokenCloze("This doesn't work", "oops"); 
+var brokenCloze("This doesn't work", "oops"); 
 
 /*
 inquirer.prompt([
@@ -165,15 +86,3 @@ inquirer.prompt([
   }
 
 })
-
-
-
-window.onload = function() {
-$(".question").append(firstPresident.front);
-$(document).on("click", ".next-one", function() {
-  // console.log(firstPresident.front); 
-  $(".question").append(firstPresident.back);
-});
-
-}
-*/
