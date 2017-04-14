fa8b7cf6c8bbfbe42beeeac8b85cdfb3850fba4c
Jared Moscrip <jaredmoscrip@gmail.com>
2017-04-14 08:46:42 -0500

    Final Commit

--- cards.js
+++ cards.js
@@ -1,18 +1,23 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
 
-
+//heres the basic card constructor that we don't need to use but we can to keep the simpletons entertained. 
+/*
 function BasicCard(front, back) {
   this.front = front;
   this.back = back;
 };
+*/
 
 function ClozeCard(text, cloze) {
+  //put the first value as the whole question
   this.text = text;
+  //second value as the answer
   this.cloze = cloze;
+  //function to render the question specially
   this.partial = function() {
     // if in the text is the text in cloze, then continue, if not throw error.
-    if (text.search( cloze ) >= 0) {
+    if (text.search(cloze) >= 0) {
      console.log(text.replace(cloze," ... "));
     } else {
      console.log("Error in the making of the card.");
@@ -24,65 +29,57 @@ function ClozeCard(text, cloze) {
   };
 };
 
-var firstPresident = new BasicCard(
-    "Who was the first president of the United States?", "George Washington");
-
-// "Who was the first president of the United States?"
-console.log(firstPresident.front); 
-
-// "George Washington"
-console.log(firstPresident.back); 
-
+//Here is the made constructors with their questions and answers parts.
 var firstPresidentCloze = new ClozeCard( 
   "George Washington was the first president of the United States.", "George Washington");
 
-// "George Washington"
-firstPresidentCloze.clozes; 
+var questionOne = new ClozeCard(
+  "Steve Jobs invented the internet", "Steve Jobs");
 
-// " ... was the first president of the United States.
+var questionTwo = new ClozeCard(
+  "Bill Gates invented the computer", "Bill Gates");
+
+var questionThree = new ClozeCard(
+  "Obama destroyed the internet.", "Obama");
+
+var questionFour = new ClozeCard(
+  "This Coding Boot Camp is fun and hard at the same time", "Coding Boot Camp")
+
+var questionFive = new ClozeCard(
+  "Randy is the best teacher I've ever had", "Randy");
+
+//here is where we have the partial question show in the console.
 firstPresidentCloze.partial(); 
+questionOne.partial();
+questionTwo.partial();
+questionThree.partial();
+questionFour.partial();
+questionFive.partial();
 
-// "George Washington was the first president of the United States.
-firstPresidentCloze.fullText();
-
-// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-var brokenCloze("This doesn't work", "oops"); 
-
-/*
 inquirer.prompt([
-
-  // Here we create a basic text prompt.
-  {
-    type: "input",
-    message: firstPresidentCloze.partial(),
-    name: "name"
-  },
-
+  // Here we have them read the question, then have the option to see the answers upon hitting the enter key.
   {
     type: "confirm",
-    message: "You want the answer?",
+    message: "You want the answers?",
     name: "confirm",
     default: true
 
-  }
-
-  
+  }  
 
 ]).then(function(user) {
-  if (user.name === true) {
-    firstPresidentCloze.cloze();
-    inquirer.prompt([
-
-    {
-      type: "confirm",
-      message: "Next question??",
-      name:"forsure",
-      default: true
-    }
-
-      ]).then(function(back) {
-        console.log("next question should be here.");
-    })
+  if (user.confirm === true) {
+    //here we just display all the answers cuz we can.
+    console.log(firstPresidentCloze.cloze);
+    console.log("-------------------------");
+    console.log(questionOne.cloze);
+    console.log("-------------------------");
+    console.log(questionTwo.cloze);
+    console.log("-------------------------");
+    console.log(questionThree.cloze);
+    console.log("-------------------------");
+    console.log(questionFour.cloze);
+    console.log("-------------------------");
+    console.log(questionFive.cloze);
   }
 
 })
